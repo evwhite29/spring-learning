6ece4beb1d73065c366c24c1000c493248edd561
Evan White <evwhite2993@gmail.com>
2018-12-12 16:53:35 -0800

    Add new string properties to CricketCoach class to experience injecting literal values via the property attribute of the bean definition in the spring config file. This required specifying the derived type when retrieving the bean from the container since these properties are unique to this one class.

--- src/main/java/com/evwhite/springlearning/SpringLearningApp.java
+++ src/main/java/com/evwhite/springlearning/SpringLearningApp.java
@@ -1,6 +1,7 @@
 package com.evwhite.springlearning;
 
 import com.evwhite.springlearning.coach.Coach;
+import com.evwhite.springlearning.coach.CricketCoach;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class SpringLearningApp {
@@ -12,13 +13,17 @@ public class SpringLearningApp {
 
         //retrieve beans from the Spring container, specifying interface type instead of derived type, Spring handles casting
         Coach theCoach = context.getBean("myCoach", Coach.class);
-        Coach cricketCoach = context.getBean("myCricketCoach", Coach.class);
+        CricketCoach cricketCoach = context.getBean("myCricketCoach", CricketCoach.class);
 
         //Utilize beans
         System.out.println(theCoach.getDailyWorkout());
         System.out.println(theCoach.getDailyFortune());
         System.out.println(cricketCoach.getDailyWorkout());
         System.out.println(cricketCoach.getDailyFortune());
+
+        //View injected literal values on cricket coach
+        System.out.println("Email: " + cricketCoach.getEmailAddress());
+        System.out.println("Team: " + cricketCoach.getTeam());
 
         //Close context
         context.close();
--- src/main/java/com/evwhite/springlearning/coach/CricketCoach.java
+++ src/main/java/com/evwhite/springlearning/coach/CricketCoach.java
@@ -5,6 +5,8 @@ import com.evwhite.springlearning.fortune.FortuneService;
 public class CricketCoach implements Coach {
 
     private FortuneService fortuneService;
+    private String emailAddress;
+    private String team;
 
     public String getDailyWorkout() {
         return "Crack that ball with that weird bat.";
@@ -16,5 +18,21 @@ public class CricketCoach implements Coach {
 
     public void setFortuneService(FortuneService fortuneService) {
         this.fortuneService = fortuneService;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setTeam(String team) {
+        this.team = team;
     }
 }
